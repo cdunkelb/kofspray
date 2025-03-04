9c9a94d01fa1b78db4cfa50d2ce9fe6d17b4fbf8
Carl Dunkelberger <39133599+cdunkelb@users.noreply.github.com>
2025-03-04 14:21:33 -0500

    remove .txt

--- create-external-dns-user.sh
+++ create-external-dns-user.sh
@@ -48,7 +48,7 @@ aws iam attach-user-policy --user-name "externaldns" --policy-arn $POLICY_ARN
 SECRET_ACCESS_KEY=$(aws iam create-access-key --user-name "externaldns")
 ACCESS_KEY_ID=$(echo $SECRET_ACCESS_KEY | jq -r '.AccessKey.AccessKeyId')
 
-cat <<-EOF > external-dns-aws-credentials.txt
+cat <<-EOF > external-dns-aws-credentials
 
 [default]
 aws_access_key_id = $(echo $ACCESS_KEY_ID)
