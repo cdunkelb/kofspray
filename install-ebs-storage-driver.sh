723ebf8168868123d6f5781f743febcd30d7206d
Carl Dunkelberger <39133599+cdunkelb@users.noreply.github.com>
2025-03-04 13:53:39 -0500

    update install

--- install-ebs-storage-driver.sh
+++ install-ebs-storage-driver.sh
@@ -13,11 +13,8 @@ fi
 helm repo add aws-ebs-csi-driver https://kubernetes-sigs.github.io/aws-ebs-csi-driver
 helm repo update
 
-helm install aws-ebs-csi-driver aws-ebs-csi-driver/aws-ebs-csi-driver \
-    --namespace kube-system \
-    --set image.repository=amazon/aws-ebs-csi-driver \
-    --set controller.serviceAccount.create=true \
-    --set node.serviceAccount.create=true
+helm upgrade --install aws-ebs-csi-driver aws-ebs-csi-driver/aws-ebs-csi-driver \
+    --namespace kube-system
 
 cat >storageclass.yaml <<EOF
 apiVersion: storage.k8s.io/v1
