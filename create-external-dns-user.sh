21c9a6cb29a2cb89b9c9a875a75134bb4041f562
Carl Dunkelberger <39133599+cdunkelb@users.noreply.github.com>
2025-03-04 14:10:54 -0500

    add script to create user for external DNS

---
+++ create-external-dns-user.sh
@@ -0,0 +1,50 @@
+cat >policy.json <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ChangeResourceRecordSets"
+      ],
+      "Resource": [
+        "arn:aws:route53:::hostedzone/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ListHostedZones",
+        "route53:ListResourceRecordSets",
+        "route53:ListTagsForResource"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+
+aws iam create-policy --policy-name "AllowExternalDNSUpdates" --policy-document file://policy.json
+
+# example: arn:aws:iam::XXXXXXXXXXXX:policy/AllowExternalDNSUpdates
+export POLICY_ARN=$(aws iam list-policies \
+ --query 'Policies[?PolicyName==`AllowExternalDNSUpdates`].Arn' --output text)
+
+ # create IAM user
+aws iam create-user --user-name "externaldns"
+
+# attach policy arn created earlier to IAM user
+aws iam attach-user-policy --user-name "externaldns" --policy-arn $POLICY_ARN
+
+SECRET_ACCESS_KEY=$(aws iam create-access-key --user-name "externaldns")
+ACCESS_KEY_ID=$(echo $SECRET_ACCESS_KEY | jq -r '.AccessKey.AccessKeyId')
+
+cat <<-EOF > external-dns-aws-credentials.txt
+
+[default]
+aws_access_key_id = $(echo $ACCESS_KEY_ID)
+aws_secret_access_key = $(echo $SECRET_ACCESS_KEY | jq -r '.AccessKey.SecretAccessKey')
+EOF
+
