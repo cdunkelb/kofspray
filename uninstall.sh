f560165e2111c50d74f2e1a10147d8ee0104e0f3
Carl Dunkelberger <39133599+cdunkelb@users.noreply.github.com>
2025-04-28 13:43:00 -0400

    change env to example user and delete clds on cleanup

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -x
 
+#delete all cluster deployments
+kubectl delete cld --all --wait -A
+
 helm uninstall --wait --cascade foreground -n kof kof-mothership
 helm uninstall --wait --cascade foreground -n kof kof-operators
 kubectl delete namespace kof --wait --cascade=foreground
